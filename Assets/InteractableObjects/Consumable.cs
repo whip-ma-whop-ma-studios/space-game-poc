0db621ab3c8ccedc118fa502eeb6bc3ce6d7af2d
ryanlaycock <ryan.laycock@hotmail.co.uk>
2021-06-10 22:21:11 +0100

    Removing flowers once they've been eaten

--- Assets/InteractableObjects/Consumable.cs
+++ Assets/InteractableObjects/Consumable.cs
@@ -7,5 +7,6 @@ public class Consumable : MonoBehaviour
     public void Consume(float healthValue)
     {
         Debug.Log("You ate it, got " + healthValue + " health!");
+        gameObject.SetActive(false);
     }
 }
