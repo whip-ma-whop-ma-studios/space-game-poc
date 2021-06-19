49bb172d90d50b50aff05f180c8b3416dc690ef2
CalebDevonport <calebdevonport@outlook.com>
2021-06-19 02:06:26 +0100

    Added dialogue system, added lots of test animations

---
+++ Assets/Scripts/Dialogue/Conversation.cs
@@ -0,0 +1,22 @@
+#pragma warning disable 0649
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Conversation", menuName = "Dialogue/New Conversation")]
+public class Conversation : ScriptableObject
+{
+    public Speaker _speakerOne;
+    public Speaker _speakerTwo;
+
+    [SerializeField]
+    private DialogueLine[] _dialogueLines;
+
+    public DialogueLine GetLineByIndex(int index)
+    {
+        return _dialogueLines[index];
+    }
+
+    public int GetLength()
+    {
+        return _dialogueLines.Length;
+    }
+}
--- Assets/Scripts/GroundCheck.cs
+++ Assets/Scripts/GroundCheck.cs
@@ -7,6 +7,8 @@ public class GroundCheck : MonoBehaviour
     [SerializeField]
     private float boxHeight = 0.05f;
     [SerializeField]
+    private float boxWidthMultiplier = 0.9f;
+    [SerializeField]
     private LayerMask groundLayer;
 
     private Vector2 playerSize;
@@ -15,7 +17,20 @@ public class GroundCheck : MonoBehaviour
     void Start()
     {
         playerSize = GetComponent<BoxCollider2D>().size;
-        boxSize = new Vector2(playerSize.x, boxHeight);
+        boxSize = new Vector2(playerSize.x * boxWidthMultiplier, boxHeight);
+    }
+
+    void OnDrawGizmos()
+    {
+        Vector2 boxCenter = (Vector2)transform.position + Vector2.down * (playerSize.y + boxHeight) / 2;
+        var left = boxCenter.x - boxSize.x / 2;
+        var right = boxCenter.x + boxSize.x / 2;
+        var top = boxCenter.y + boxSize.y / 2;
+        var bottom = boxCenter.y - boxSize.y / 2;
+        Gizmos.DrawLine(new Vector3(left, top), new Vector3(left, bottom));
+        Gizmos.DrawLine(new Vector3(left, bottom), new Vector3(right, bottom));
+        Gizmos.DrawLine(new Vector3(right, bottom), new Vector3(right, top));
+        Gizmos.DrawLine(new Vector3(right, top), new Vector3(left, top));
     }
 
     void Update()
